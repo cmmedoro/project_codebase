d4cb3c94ccc0b35930e228290a42f64cfc73cbbe
s271882@studenti.polito.it <s271882@studenti.polito.it>
2023-04-29 12:11:00 +0200

    Change in optimizers parameters

--- main.py
+++ main.py
@@ -65,11 +65,11 @@ class LightningModel(pl.LightningModule):
         if self.opt_name.lower() == "sgd":
             optimizers = torch.optim.SGD(self.parameters(), lr=0.001, weight_decay=0.001, momentum=0.9)
         if self.opt_name.lower() == "adamw":
-            optimizers = torch.optim.AdamW(self.parameters(), lr=0.001, weight_decay=0.001, momentum=0.9)
+            optimizers = torch.optim.AdamW(self.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.01)
         if self.opt_name.lower() == "adam":
-            optimizers = torch.optim.Adam(self.parameters(), lr=0.001, weight_decay=0.001, momentum=0.9)
+            optimizers = torch.optim.Adam(self.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
         if self.opt_name.lower() == "asgd":
-            optimizers = torch.optim.ASGD(self.parameters(), lr=0.001, weight_decay=0.001, momentum=0.9)
+            optimizers = torch.optim.ASGD(self.parameters(), lr=0.01, lambd=0.0001, alpha=0.75, t0=1000000.0, weight_decay=0)
         return optimizers
 
     #  The loss function call (this method will be called at each training iteration)
