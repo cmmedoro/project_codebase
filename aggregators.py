fd9bc1b9336a81bc9b9fd7f6a664e14c71473bc4
s271882@studenti.polito.it <s271882@studenti.polito.it>
2023-05-17 11:32:42 +0200

    added explanatory comments

--- aggregators.py
+++ aggregators.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 import self_modules as sm
 
 def get_aggregator(agg_arch, agg_config={}):
-
+    #return the aggregator and the corresponding parameters for the configuration
     if 'gem' in agg_arch.lower():
         agg_config={'p': 3}
         return sm.GeM(**agg_config)
@@ -20,13 +20,25 @@ def get_aggregator(agg_arch, agg_config={}):
         return sm.MixVPR(**agg_config)
     
 class Flatten(torch.nn.Module):
-    def __init__(self): super().__init__()
-    def forward(self, x): assert x.shape[2] == x.shape[3] == 1; return x[:,:,0,0]
+    def __init__(self): 
+        super().__init__()
+
+    def forward(self, x): 
+        #assert is a debugging statement ---> checks if the condition presented is True or not
+        #in this case: if the third and fourth dimensions of the descriptors tensor are equal and are equal to 1
+        #this function returns a descriptor vector flattened, meaning that it will have a dimension of the kind
+        #(num_batches, 512)
+        assert x.shape[2] == x.shape[3] == 1;       
+        return x[:,:,0,0]
 
 class L2Norm(torch.nn.Module):
     def __init__(self, dim=1):
         super().__init__()
+        #consider transforming the vector along dimension dim
         self.dim = dim
     
     def forward(self, x):
+        # Perform L2 normalization, meaning normalize the descriptors vector with respect to the
+        # maximum between all the two-norms (sum of squared terms) of the vector --> so that
+        # the sum of squared terms is always equal to 1
         return F.normalize(x, p=2.0, dim=self.dim)
--- self_modules.py
+++ self_modules.py
@@ -7,13 +7,23 @@ import numpy as np
 class GeM(nn.Module):
     def __init__(self, p=3, eps=1e-6):
         super(GeM,self).__init__()
+        # Define a tensor to be considered as a parameter of class nn.Module, so it will be available in parameters()
+        # tensor of size 1 filled with ones*p (overall value: 3)
+        # requires_grad: specify if the parameter requires the gradient
         self.p = nn.Parameter(torch.ones(1)*p, requires_grad=True)
         self.eps = eps
 
     def forward(self, x):
+        # call the gem function and apply it to the descriptors
         return self.gem(x, p=self.p, eps=self.eps)
         
     def gem(self, x, p=3, eps=1e-6):
+        # define the gem operation as follows:
+        # apply a 2D average pooling operation in kH x kW regions by step size sH x sW steps
+        # input: descriptors limited in range [1e-6, inf) and we consider the values at the power of 3
+        # kernel size is a tuple considering size of the second to last dimension and the last one
+            # ---> pooling region (kH, kW)
+        # the result f the average pooling operation is elevated at the power of 1/3
         return F.avg_pool2d(x.clamp(min=eps).pow(p), (x.size(-2), x.size(-1))).pow(1./p)
         
     def __repr__(self):
@@ -24,19 +34,30 @@ class FeatureMixerLayer(nn.Module):
     def __init__(self, in_dim, mlp_ratio=1):
         super(FeatureMixerLayer, self).__init__()
         self.mix = nn.Sequential(
-            nn.LayerNorm(in_dim), #49
-            nn.Linear(in_dim, int(in_dim * mlp_ratio)), #(49, 49*1)
+            # apply Layer Normalization considering an input dimension of 49
+            nn.LayerNorm(in_dim), 
+            # apply fc layer with input_features = 49 and output features 49*1
+            nn.Linear(in_dim, int(in_dim * mlp_ratio)),
+            # apply ReLU activation function: does not change the output dimension with respect to the input one
             nn.ReLU(),
-            nn.Linear(int(in_dim * mlp_ratio), in_dim), #(49, 49)
+            # apply fc layer with input_features = 49*1 and output features
+            nn.Linear(int(in_dim * mlp_ratio), in_dim), 
         )
-
+        #in Figure 2 of the paper mixVPr we can see that the feature mixer(that will be repeated L times) is composed 
+        # exactly by these stages: normalization, projection, activation and projection
+        # self.modules() = iterator over all the modules of the network
         for m in self.modules():
             if isinstance(m, (nn.Linear)):
+                # if we are considering a fc layer, initialize the parameters, so that they will not be considered by autograd.
+                # Fill the tensor of the weights of the layer with values from a truncated Normal distribution with standard deviation
+                # 0.02 and mean 0.0, truncated at bounds -2.0 and 2.0
                 nn.init.trunc_normal_(m.weight, std=0.02)
                 if m.bias is not None:
+                    # if the biases of the layer are not None, fill them with 0s
                     nn.init.zeros_(m.bias)
 
     def forward(self, x):
+        # equation 2 of MixVPR paper
         return x + self.mix(x)
 
 
@@ -63,31 +84,40 @@ class MixVPR(nn.Module):
         self.mlp_ratio = mlp_ratio # ratio of the mid projection layer in the mixer block
 
         hw = in_h*in_w #7*7 = 49
+        # define a mix layer as a series of L = 4 FeatureMizerLayer
         self.mix = nn.Sequential(*[
             FeatureMixerLayer(in_dim=hw, mlp_ratio=mlp_ratio)
             for _ in range(self.mix_depth)
         ])
-        self.channel_proj = nn.Linear(in_channels, out_channels)  #(512, 512)
-        self.row_proj = nn.Linear(hw, out_rows) #(49, 4)
+        # define a channel projection operation ---> fc layer considering (512, 512)
+        self.channel_proj = nn.Linear(in_channels, out_channels)
+        # define a row projection operation ---> fc layer considering (49, 4)
+        self.row_proj = nn.Linear(hw, out_rows)
 
     def forward(self, x):
         #Input: (num_batches, 512, 7, 7)
         x = x.flatten(2)
-        #intermediate_output: (256, 512, 49)
+        # for each image (size 0) sent to the backbone obtains c channels (size 1) of dimension n = h * w ---> flatten third and fourth dim
+        #intermediate_output: (256, 512, 49) ---> (batch_size, c, n)
         x = self.mix(x)
         #intermediate_output: (256, 512, 49)
+        # Now we want to reduce the dimensionality through channel wise and row wise projections with two fc layers
         x = x.permute(0, 2, 1) #permute dimensions, invert second and third dimension
-        #intermediate_output: (256, 49, 512)
+        #intermediate_output: (256, 49, 512) ---> (batch_size, n, c)
+        # Reduce number of channels from in_channels to out_channels
         x = self.channel_proj(x)
-        #intermediate_output: (256, 49, 512)
+        #intermediate_output: (256, 49, 512) ---> (batch_size, n, out_c)
         x = x.permute(0, 2, 1)
-        #intermediate_output: (256, 512, 49)
+        #intermediate_output: (256, 512, 49) ---> (batch_size, out_c, n)
+        # Reduce number of rows from h*w to out_rows
         x = self.row_proj(x)
         #intermediate_output: (256, 512, 4)
+        # x.flatten(1) ---> consider the descriptors flattened to (batch_size, out_c * out_rows), then apply on this L2 normalization
         x = F.normalize(x.flatten(1), p=2, dim=-1)
         #output: (256, 2048)
         return x
     """
+        
 #def print_nb_params(m):
  #   model_parameters = filter(lambda p: p.requires_grad, m.parameters())
   #  params = sum([np.prod(p.size()) for p in model_parameters])
