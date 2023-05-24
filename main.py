29688c45af0b09e2e950be12ca6602d19d55470b
s271882@studenti.polito.it <s271882@studenti.polito.it>
2023-05-24 17:26:18 +0200

    val_loss as monitor

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ class LightningModel(pl.LightningModule):
         #if(scheduler is None):
         #    return optimizers
         #return [optimizers], scheduler
-        return {"optimizer": optimizers, "lr_scheduler": scheduler, "monitor" : "metric_to_track"}
+        return {"optimizer": optimizers, "lr_scheduler": scheduler, "monitor" : "val_loss"}
     #{"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "metric_to_track"}
 
 
